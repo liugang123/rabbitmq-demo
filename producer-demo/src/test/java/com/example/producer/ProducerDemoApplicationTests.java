86888f79fe5b9a847bee2e6e65f4d4dc73e8fb93
liugang19 <liugang19@xdf.cn>
2020-11-18 12:05:09 +0800

    create project 1118

---
+++ producer-demo/src/test/java/com/example/producer/ProducerDemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
+@SpringBootTest
+class ProducerDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
